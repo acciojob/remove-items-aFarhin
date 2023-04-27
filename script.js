73c2b0385fdf54ece66e4ba967bf5122d0eedf5b
Ahmeda Farhin <124598487+aFarhin@users.noreply.github.com>
2023-04-27 13:12:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+ // script.js file
+function removeColor() {
+  const select = document.getElementById("colorSelect");
+  const selectedIndex = select.selectedIndex;
+  select.options[selectedIndex].remove();
+}
